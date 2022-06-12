9ebb643163e6fbac26cd41c453db39b67560c0b9
laureenakinyi <laureen.akinyi@student.moringaschool.com>
2022-06-12 18:01:02 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(plan = "roller-skate"){
+   return `This Saturday, I want to ${plan}!`
+}
+saturdayFun();
+
+function mondayWork(event = "go to the office"){
+   return `This Monday, I will ${event}.`
+}
+ 
+mondayWork("This Monday, I will work from work.")
+
+function wrapAdjective(adjective){
+   const specialChar = function(start){
+      return `You are ${adjective}${start}${adjective}!`
+   } 
+   return specialChar
+}
+wrapAdjective("*")
